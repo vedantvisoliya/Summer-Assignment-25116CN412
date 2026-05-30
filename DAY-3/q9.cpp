12632f79e0baf69e486c4bed31c52b536304e524
Vedant Kumar <vedantvisoliya@gmail.com>
2026-05-30 21:25:30 +0530

    fixed bug in question no 9

--- DAY-3/q9.cpp
+++ DAY-3/q9.cpp
@@ -6,6 +6,10 @@ int main(){
     cout << "Enter a number: " ;
     cin >> num;
 
+    if (num == 1) {
+        flag++;
+    }
+
     for (i = 2; i <= num/2; i++) {
         if (num % i == 0) {
             flag = 1;
